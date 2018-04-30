04286fabe5c9acfe226542e4a49532a17f69c5ae
Katie Cassell <kcassell944@gmail.com>
2018-04-30 19:17:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,23 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    scraped_students = []
+    doc.css(".student-card").each do |student|
+      student_hash = {}
+      student_hash[:name] = student.css(".student-name").text
+      student_hash[:location] = student.css(".student-location").text
+      student_hash[:profile_url] = student.css("a").attribute("href").value
+      scraped_students << student_hash
+    end
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+      doc = Nokogiri::HTML(open(profile_url))
+      student_hash = {}
+      doc.css(".social-icon-container")
+    end
+
 
 end
-
