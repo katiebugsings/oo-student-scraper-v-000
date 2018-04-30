0dd12d9a64c873c440264817a70a74e040db2b05
Katie Cassell <kcassell944@gmail.com>
2018-04-30 19:25:34 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,7 +19,8 @@ class Scraper
   def self.scrape_profile_page(profile_url)
       doc = Nokogiri::HTML(open(profile_url))
       student_hash = {}
-      doc.css(".social-icon-container")
+      links = doc.css(".social-icon-container")
+      binding.pry
       student_hash
     end
 
